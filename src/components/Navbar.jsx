f5c162b962b80b15c1f6474d6ee242e59a950d41
Suraj Chaudhari <theprogrammer0086@gmail.com>
2024-09-24 14:07:36 +0530

    setting up deploy error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import navData from '../details.json';
 import '../styles/menuBtn.css';
